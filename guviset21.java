a163328dfeeaf00e9f951dda5c059cba8c79d13b
jawaharkutty <40166747+jawaharkutty@users.noreply.github.com>
2018-07-28 15:02:25 +0530

    Create guviset21.java

---
+++ guviset21.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class guviset21
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int N1=sc.nextInt();
+        int N2=sc.nextInt();
+        System.out.print((int)Math.pow(N1,N2));
+        
+    }
+}
