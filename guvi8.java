f9fa1859d23b2295ee98aa2c89e79bbbe18c487d
jawaharkutty <40166747+jawaharkutty@users.noreply.github.com>
2018-07-28 14:25:28 +0530

    Create guvi8.java

---
+++ guvi8.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class guvi8
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int no=0;
+        for(int i=1;i<=n;i++)
+        {
+            no+=i;
+        }
+        System.out.print(no);
+    }
+}
