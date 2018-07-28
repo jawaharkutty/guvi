41a5ceceaa4ea202edff74431bc94ea96c25c93b
jawaharkutty <40166747+jawaharkutty@users.noreply.github.com>
2018-07-28 05:03:58 +0000

    Create guvi6.java

---
+++ guvi6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class guvi6
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int num= sc.nextInt();
+        if(num%4==0)
+        System.out.print("yes");
+        else
+        System.out.print("no");
+    }
+}
